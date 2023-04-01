cc05fc0c0b5676904bae158e2ac327f873ba9440
guohaichen <1299117865@qq.com>
2023-04-02 00:50:25 +0800

    登录返回增加了用户信息

---
+++ src/main/java/com/chen/common_service/vo/UserInfo.java
@@ -0,0 +1,17 @@
+package com.chen.common_service.vo;
+
+import lombok.Data;
+
+/**
+ * 返回用户信息对象
+ */
+@Data
+public class UserInfo {
+    private String username;
+
+    private String nickname;
+
+    private String phone;
+
+    private String token;
+}
