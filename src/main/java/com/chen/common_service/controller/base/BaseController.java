a4771022413ab023e12526d7cf029ec02260b91d
guohai <1299117865@qq.com>
2023-07-27 17:54:11 +0800

    测试分片上传，合并分片

--- src/main/java/com/chen/common_service/controller/base/BaseController.java
+++ src/main/java/com/chen/common_service/controller/base/BaseController.java
@@ -9,18 +9,18 @@ import com.aliyun.oss.model.PutObjectResult;
 import com.chen.common_service.dto.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author cgh
@@ -32,8 +32,8 @@ import java.util.Date;
 public class BaseController {
 
     private final static String endpoint = "https://oss-cn-shanghai.aliyuncs.com";
-    private final static String accessKeyId = "xxxxxxxxxxxxxxxxxxxx";
-    private final static String accessKeySecret = "xxxxxxxxxxxxxxxxxxxxxx";
+    private final static String accessKeyId = "LTAI5tJmphKDCS4gB1o1vAM9";
+    private final static String accessKeySecret = "OckTkiQZPpVIuC0506fxOhqYUsApTQ";
     private final static String bucketName = "guohai-test";
     private final static String key = "<downloadKey>";
     private final static String uploadFile = "<uploadFile>";
@@ -41,10 +41,19 @@ public class BaseController {
     @Value(value = "${img.baseSrc}")
     private String uploadPath;
 
+    //分片文件临时存储目录
+    public final Path uploadFileTempPath = Paths.get("F:\\gitHub\\blog\\temp\\");
+
+    public BaseController() {
+    }
+
     @PostMapping("/uploadFile")
     //文件上传至服务器
     public Result<?> uploadImg(@RequestParam(value = "file") MultipartFile multipartFile, HttpServletRequest request) {
         //获取文件名
+        if (request.getParameter("path") == null) {
+            return Result.error("上传路径不能为空");
+        }
         String path = request.getParameter("path");
         File filePrePath = new File(uploadPath + File.separator + path + File.separator);
         //文件夹不存在，创建文件夹
@@ -118,6 +127,70 @@ public class BaseController {
         }
     }
 
+    /**
+     * @param file         文件
+     * @param filename     文件名
+     * @param contentRange 文件字符
+     *                     分片保存多个文件
+     */
+    @PostMapping("/bigFile")
+    public Result<?> bigFileUpload(@RequestParam("file") MultipartFile file,
+                                   @RequestParam("filename") String filename,
+                                   @RequestHeader("Content-Range") String contentRange) {
+        try {
+            final Path uploadFireDirectory = Paths.get(uploadPath);
+            //解析content-range头部，获取分片的起始结束字节位置
+            String[] rangeParts = contentRange.split("[ -/]");
+            long startByte = Long.parseLong(rangeParts[1]);
+
+            //分片文件上传临时目录，以fileName和起始字节位置作为文件名,先将文件前后缀分开
+            String filenamePrefix = filename.substring(0, filename.lastIndexOf("."));
+            String fileSuffix = filename.substring(filename.lastIndexOf(".") + 1);
+
+            Path tempFilePath = uploadFireDirectory.resolve(filenamePrefix + "-" + startByte +"."+ fileSuffix);
+            Files.write(tempFilePath, file.getBytes());
+            return Result.OK("分片上传成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Result.error("分片上传失败");
+        }
+    }
+
+    /**
+     * 合并分片文件
+     *
+     * @param filename 文件名称
+     * @return
+     */
+    @PostMapping("/bingFile/merge")
+    public Result<?> mergeFile(@RequestParam("file") String filename) {
+        //目标存储目录
+        Path targetFilePath = Paths.get(uploadPath + File.separator).resolve(filename);
+        //获取临时目录下所有的分片文件
+        try {
+            List<Path> tempFiles = Files.list(uploadFileTempPath)
+                    .filter(file -> file.getFileName().toString().startsWith(filename))
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            //合并分片文件到目标文件夹
+            try {
+                OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(targetFilePath.toFile().toPath()));
+                for (Path tempFile : tempFiles) {
+                    byte[] data = Files.readAllBytes(tempFile);
+                    outputStream.write(data);
+                    Files.delete(tempFile);
+                }
+                Files.delete(uploadFileTempPath);
+                return Result.OK("文件合并成功");
+            } catch (IOException e) {
+                return Result.error("文件合并失败:\t" + e.getMessage());
+            }
+        } catch (IOException e) {
+            return Result.error("文件合并失败:\t" + e.getMessage());
+        }
+    }
+
     //拼接路径+文件名
     private String setOSSFilePath(String fileName) {
         Date date = new Date();
@@ -126,7 +199,8 @@ public class BaseController {
         // 2023-07-06 ----> 2023/07/06
         String fileDirectory = formatDate.replaceAll("-", "/");
 
-        log.info("文件目录:{},文件名称:{} ", fileDirectory,fileName);
+        log.info("文件目录:{},文件名称:{} ", fileDirectory, fileName);
         return fileDirectory + "/" + fileName;
     }
+
 }
